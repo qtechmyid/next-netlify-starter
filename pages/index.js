4f705f9c5f611966cd3bbd4a29ae1ab89af7d53f
Qiannah ayudisameida <99195271+qtechmyid@users.noreply.github.com>
2022-03-04 00:13:59 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Get started <a href="https://qtech.id">qtech</a> by editing <code>pages/index.js</code>
         </p>
     <a href="https://qtech.id">qtech</a>
       </main>
